9f15168a58fd3ed7b88c2fc6f25e5d10e0eeeab8
Ray <r.v.toll@jio.nl>
2025-06-04 12:38:29 +0200

    move positions to /

--- project/apps/core/urls/urls.py
+++ project/apps/core/urls/urls.py
@@ -6,7 +6,6 @@ from drf_spectacular.views import (
 
 from django.urls import include, path
 
-from project.apps.core.views import PositionListView
 
 urlpatterns = [
     path("schema/", SpectacularAPIView.as_view(), name="schema"),
@@ -14,6 +13,5 @@ urlpatterns = [
         "swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"
     ),
     path("redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
-    path("position_list/", PositionListView.as_view(), name="positions_read_only"),
     path("", include("project.apps.core.urls.viewsets")),
 ]
--- project/urls.py
+++ project/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from project.apps.core.views import PositionListView
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("project.apps.core.urls")),
+    path("", PositionListView.as_view(), name="positions_read_only"),
 ]
